742cba36b1744f5d5023a12e0e44cc1a9d0d1b41
mannumishra <mannumishra2207@outlook.com>
2024-07-29 22:18:51 +0530

    ad chmage

--- DB/Db.js
+++ DB/Db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectdb = async()=>{
     try {
-        await mongoose.connect("mongodb://localhost:27017/barcode")
+        await mongoose.connect("mongodb+srv://mannu22072000:3ifVsxZDk49nx6aa@cluster0.b7vw3na.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("Database connected successfully")
     } catch (error) {
         console.log(error)
